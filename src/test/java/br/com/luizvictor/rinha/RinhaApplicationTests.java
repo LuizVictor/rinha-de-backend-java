6fa0aabbab4178bafa2c9c24fc335e97fe4cc2bb
Luiz Victor <luizvictor.dev@outlook.com>
2023-12-25 19:19:08 -0300

    first commit

---
+++ src/test/java/br/com/luizvictor/rinha/RinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizvictor.rinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
